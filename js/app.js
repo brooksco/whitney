2ddd2ce717a88d306be4b76ac41534f82146ee32
Colin Brooks <colinbooks@gmail.com>
2016-04-10 20:29:40 -0400

    reverting back to regular open studio form

--- js/app.js
+++ js/app.js
@@ -430,9 +430,9 @@ function processForm(e) {
 	var hearAbout = $("input[name='How did you hear about Open Studio for Teens?']:checked").val();
 	var grade = $("input[name='Grade']:checked").val();
 	var timesWhitney = $("input[name='How many times have you been to the Whitney?']:checked").val();
-	// var timesStudio = $("input[name='How many times have you attended Open Studio?']:checked").val();
+	var timesStudio = $("input[name='How many times have you attended Open Studio?']:checked").val();
 	// Blank for open studio event
-	var timesStudio = '';
+	// var timesStudio = '';
 
 	var emailValid = emailValidator(email);
 
@@ -487,9 +487,9 @@ function pushToGoogle(time) {
 
 	$.ajax({
 		// Teen event//test ajax form
-		url: "https://script.google.com/macros/s/AKfycbzP-0Yap6_ATFVXKyEubucQRmiI0E8hdSRLl8bocpLassYXr5Iy/exec",
+		// url: "https://script.google.com/macros/s/AKfycbzP-0Yap6_ATFVXKyEubucQRmiI0E8hdSRLl8bocpLassYXr5Iy/exec",
 		// Regular whitney open studio form
-		// url: "https://script.google.com/macros/s/AKfycbwn9UxQvWSfxuCj8wdP3WVgusPlWCfUYuSTKTOk40Brw3ceZiU/exec",
+		url: "https://script.google.com/macros/s/AKfycbwn9UxQvWSfxuCj8wdP3WVgusPlWCfUYuSTKTOk40Brw3ceZiU/exec",
 		type: "post",
 		data: serializedData,
 		statusCode: {
@@ -592,6 +592,90 @@ var substringMatcher = function(strs) {
     cb(matches);
 };
 };
+
+
+// function exportTableToCSV($table, filename) {
+//                 var $headers = $table.find('tr:has(th)')
+//                     ,$rows = $table.find('tr:has(td)')
+
+//                     // Temporary delimiter characters unlikely to be typed by keyboard
+//                     // This is to avoid accidentally splitting the actual contents
+//                     ,tmpColDelim = String.fromCharCode(11) // vertical tab character
+//                     ,tmpRowDelim = String.fromCharCode(0) // null character
+
+//                     // actual delimiter characters for CSV format
+//                     ,colDelim = '","'
+//                     ,rowDelim = '"\r\n"';
+
+//                     // Grab text from table into CSV formatted string
+//                     var csv = '"';
+//                     csv += formatRows($headers.map(grabRow));
+//                     csv += rowDelim;
+//                     csv += formatRows($rows.map(grabRow)) + '"';
+
+//                     // Data URI
+//                     var csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+//                 // For IE (tested 10+)
+//                 if (window.navigator.msSaveOrOpenBlob) {
+//                     var blob = new Blob([decodeURIComponent(encodeURI(csv))], {
+//                         type: "text/csv;charset=utf-8;"
+//                     });
+//                     navigator.msSaveBlob(blob, filename);
+//                 } else {
+//                     $(this)
+//                         .attr({
+//                             'download': filename
+//                             ,'href': csvData
+//                             //,'target' : '_blank' //if you want it to open in a new window
+//                     });
+//                 }
+
+//                 //------------------------------------------------------------
+//                 // Helper Functions 
+//                 //------------------------------------------------------------
+//                 // Format the output so it has the appropriate delimiters
+//                 function formatRows(rows){
+//                     return rows.get().join(tmpRowDelim)
+//                         .split(tmpRowDelim).join(rowDelim)
+//                         .split(tmpColDelim).join(colDelim);
+//                 }
+//                 // Grab and format a row from the table
+//                 function grabRow(i,row){
+                     
+//                     var $row = $(row);
+//                     //for some reason $cols = $row.find('td') || $row.find('th') won't work...
+//                     var $cols = $row.find('td'); 
+//                     if(!$cols.length) $cols = $row.find('th');  
+
+//                     return $cols.map(grabCol)
+//                                 .get().join(tmpColDelim);
+//                 }
+//                 // Grab and format a column from the table 
+//                 function grabCol(j,col){
+//                     var $col = $(col),
+//                         $text = $col.text();
+
+//                     return $text.replace('"', '""'); // escape double quotes
+
+//                 }
+//             }
+
+
+//             // This must be a hyperlink
+//             $("#export").click(function (event) {
+//                 // var outputFile = 'export'
+//                 var outputFile = window.prompt("What do you want to name your output file (Note: This won't have any effect on Safari)") || 'export';
+//                 outputFile = outputFile.replace('.csv','') + '.csv'
+                 
+//                 // CSV
+//                 exportTableToCSV.apply(this, [$('#dvData > table'), outputFile]);
+                
+//                 // IF CSV, don't do event.preventDefault() or return false
+//                 // We actually need this to be a typical hyperlink
+//             });
+//         });
+
 
 // Need to do a key/pair of common acronyms and real names
 // var schoolAcronyms = {
