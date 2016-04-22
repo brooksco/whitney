f73a5e7683ea40c40575ae2235a9c330c4cff348
Colin Brooks <colinbooks@gmail.com>
2016-04-21 23:41:11 -0400

    updated 404 bearror

--- 404.php
+++ 404.php
@@ -2,63 +2,96 @@
 <html>
 <head>
   <title>What is lost can be found again (404)</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style type="text/css">
-  body { 
-    background-color: #242b2b; 
-    color: white; 
-    font-family: arial, sans-serif; 
-  }
 
-  h2 { 
-    font-size: 6em; 
-    margin: 0; 
-    color: white; 
+    body { 
+      background-color: #242b2b; 
+      color: white; 
+      font-family: arial, sans-serif;
+      margin: 0;
+    }
 
-  }
+    h2 { 
+      font-size: 4em; 
+      margin: 0; 
+      color: white; 
 
-  p { 
-    font-size: 1.5em; 
-    line-height: 1.5em;
-  }
+    }
 
-  #bear-bearrier {
-   margin-top: 5em;
-   max-width: 900px;
-   margin-left: auto; 
-   margin-right: auto;
-   padding-left: 10%;
-   padding-right: 10%;
-  }
+    p { 
+      font-size: 1.5em; 
+      line-height: 1.5em;
+      margin: .5em 0em 0em 0em;
+    }
 
-  #bearror-of-bad-news {
-    padding-top: 3em;
-    padding-left: .25em;
-  }
+    #bear-bearrier {
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
 
-  #andy {
-    width: 250px;
-    float: left;
-    display: inline;
-    margin-right: 2em;
-    margin-bottom: .5em;
-  }
+      -ms-flex-align: center;
+      -webkit-align-items: center;
+      -webkit-box-align: center;
 
-  #somewhere {
-    text-decoration: none;
-    color: white;
-  }
+      align-items: center;
 
-  #somewhere:hover {
-    text-decoration: underline;
-  }
+      height: 100vh;
+      max-width: 900px;
+      margin-left: auto; 
+      margin-right: auto;
+      padding: 0 2em 0 2em;
+    }
 
-</style>
+    #andy {
+      width: 250px;
+      float: left;
+      display: inline;
+      margin: 0em 2em .5em 0em;
+    }
+
+    #somewhere {
+      text-decoration: none;
+      color: white;
+    }
+
+    #somewhere:hover {
+      text-decoration: underline;
+    }
+
+    @media (max-width: 620px) {
+      #bear-bearrier {
+        margin-top: 1.2em;
+        display: block;
+        height: auto;
+      }
+
+      #bearror-of-bad-news {
+        margin: 1.2em 0 1.2em 0;
+      }
+
+      #andy {
+        display: block;
+        float: none;
+        margin: auto;
+      }
+
+      h2 {
+        font-size: 3em;
+      }
+
+      p {
+        font-size: 1.1em;
+      }
+    }
+
+  </style>
 </head>
 
 <body>
 
   <div id="bear-bearrier">
-     <img id="andy" src="img/404littlebear.png" />
+   <img id="andy" src="img/404littlebear.png" />
 
    <div id="bearror-of-bad-news">
     <h2>Ruh roh.</h2>
